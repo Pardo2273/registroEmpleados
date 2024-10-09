4bd2f53147f6d11761d3e8421056a3c8774640b8
Pardo2273 <mpita38@gmail.com>
2024-10-09 14:38:10 -0600

    adjunto ultimas modificaciones del proyecto, se realiza la funcion mostrarEmpleados con el fin de que se brinden los elementos en una lista que se crea desde js y asimismo poner los estilos con tailwind desde el js, se brinda todo el codigo comentado para una mejor compresion

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function ingresarEmpleado(){
     }
 }
 
+//despues de guardar el empleado limpia los input
 function limpiar(){
     elementoLegajo.value ="";
     elementoNombre.value = "";
@@ -47,6 +48,7 @@ function limpiar(){
     elementoPuesto.value = "";
 }
 
+//se muestra solo cuando guardo al empleado
 function mensajeGuardado(){
     Swal.fire({
         position: "top-end",
@@ -58,5 +60,36 @@ function mensajeGuardado(){
 }
 
 
+//funcion que muestra los empleados
+function mostrarEmpleados(){
+    document.getElementById('textoEmpleado').textContent = "Empleados: "; // esto es para darle titulo al h3
+    //capturo el ul que almacenara los li
+    let lista = document.getElementById('containerLista');
+    for(let item of arrayEmpleado){// por cada item del arreglo (o sea cada objeto que esta dentro del arreglo)
+        let textoLista = "";//se vacia cada vez que entra a otro objeto, porque sino estaria concatenando absolutamente todo
+        for(let propiedadEmpleado in item){//por cada propiedad del objeto
+            textoLista += propiedadEmpleado + ": " + item[propiedadEmpleado] +", ";//brindo los valores del objeto
+        } 
 
+        //creo el conjunto de elementos que voy a emplear para desplegar la informracion del empleado, en el html quedaria asi: li>div>p> 
+        let itemLista = document.createElement("li");
+        let elementoDiv1 = document.createElement("div");
+        let elementoParraf= document.createElement("p");
+
+        //doy la clase de tailwind
+        itemLista.setAttribute("class", "flex justify-between gap-x-6 py-5");
+        elementoDiv1.setAttribute("class", "flex min-w-0 gap-x-4");
+        elementoParraf.setAttribute("class", "text-sm font-semibold leading-6 text-gray-900");
+
+        elementoParraf.innerText = textoLista;//le brindo el texto que se crea en el for in
+
+        elementoDiv1.appendChild(elementoParraf); 
+
+
+        itemLista.appendChild(elementoDiv1);//el li tiene adentro el div
+
+
+        lista.appendChild(itemLista);//brindo el hijo al ul 
+    }
+}
 
